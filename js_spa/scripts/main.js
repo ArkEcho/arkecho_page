7c659ab63f287c4a34c7d02e0a4b8d1e01128266
Stephan Ziesenis <ziesenis@stud.hs-heilbronn.de>
2017-01-28 11:36:59 +0100

    Implemented Routing between views

--- js_spa/scripts/main.js
+++ js_spa/scripts/main.js
@@ -1,5 +1,27 @@
 var mainApp = angular.module('mainApp', []);
 
+// Routing
+mainApp.config(function($routeProvider)
+{
+  $routeProvider
+    .when('/',
+      {
+        controller: 'MainController',
+        templateUrl: 'views/viewHome.html'
+      })
+    .when('/contact',
+      {
+        controller: 'MainController',
+        templateUrl: 'views/viewContact.html'
+      })
+    .when('/about',
+      {
+        controller: 'MainController',
+        templateUrl: 'views/viewAbout.html'
+      })
+    .otherwise({redirectTo: '/'});
+});
+
 // Controller Definition
 var controllers = {};
 controllers.MainController = function ($scope)
