98e394eb8b9d70a9824ba792aa0554b7e52bcf8e
Stephan Ziesenis <ziesenis@stud.hs-heilbronn.de>
2017-01-30 18:11:58 +0100

    Implemented WebSocket Controller; Small Changes

--- arkecho_page/scripts/main.js
+++ arkecho_page/scripts/main.js
@@ -25,6 +25,64 @@ mainApp.config(function($routeProvider)
 // Controller Definition
 var controllers = {};
 
+controllers.ConnectionController = function($scope)
+{
+  /*
+  enum MESSAGETYPE
+  {
+      MT_NOTDEFINED = 0,
+      MT_ECHO_TEST = 1,
+      MT_PLAY_PAUSE = 2,
+      MT_BACKWARD = 3,
+      MT_FORWARD = 4,
+      MT_REQUEST_SONG_ACTUAL = 5,
+      MT_SEND_SONG_ACTUAL = 6
+  }
+  */
+
+  var webSocket_;
+  var webSocketOpen_ = false;
+
+  openConnection('192.168.0.59:1000');
+  //sendMessage('2', '');
+
+  function openConnection(address)
+  {
+    webSocket_ = new WebSocket('ws://' + address);
+  }
+
+  function closeConnection()
+  {
+    webSocket_.close();
+  }
+
+  function sendMessage(type, message)
+  {
+    var json = '{ "Typ": ' + type + ', "Message": "' + message + '" }';
+    webSocket_.send(json);
+  }
+  /*
+  webSocket.onopen = function (evt) {
+    //alert("Connected..");
+  }
+
+  webSocket.onclose = function (evt) {
+    //alert("Disconnected...");
+  }
+
+  webSocket.onmessage = function (evt) {
+    var json = JSON.parse(evt.data);
+    var type = json.Typ;
+    var message = json.Message;
+    //alert(out);
+  }
+
+  webSocket.onerror = function (evt) {
+    alert("Error...");
+  }
+  */
+};
+
 controllers.HomeController = function($scope)
 {
 };
@@ -39,7 +97,6 @@ controllers.AboutController = function($scope)
 
 controllers.NavigationController = function ($scope)
 {
-  $scope.testMessage = 'TEST NACHRICHT TEST';
   var active = 'navigationActive';
   init(); // Aufrufen der Initialisierungs Funktion des Navigation Controllers
 
