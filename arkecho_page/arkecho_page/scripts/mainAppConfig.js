3d4e5ad63f44d07b70d629b5d33ac63f0fd9b960
Stephan Ziesenis <ziesenis@stud.hs-heilbronn.de>
2017-02-01 15:52:18 +0100

    Implemented Connect "Button" in ActionBar

--- arkecho_page/arkecho_page/scripts/mainAppConfig.js
+++ arkecho_page/arkecho_page/scripts/mainAppConfig.js
@@ -3,21 +3,21 @@ var mainApp = angular.module('mainApp', ['ngRoute']);
 // Routing
 mainApp.config(function($routeProvider)
 {
-  $routeProvider
-    .when('/',
-      {
-        controller: 'HomeController',
-        templateUrl: 'views/viewHome.html'
-      })
-    .when('/contact',
-      {
-        controller: 'ContactController',
-        templateUrl: 'views/viewContact.html'
-      })
-    .when('/about',
-      {
-        controller: 'AboutController',
-        templateUrl: 'views/viewAbout.html'
-      })
-    .otherwise({redirectTo: '/'});
+    $routeProvider
+        .when('/',
+        {
+            controller: 'HomeController',
+            templateUrl: 'views/viewHome.html'
+        })
+        .when('/contact',
+        {
+            controller: 'ContactController',
+            templateUrl: 'views/viewContact.html'
+        })
+        .when('/about',
+        {
+            controller: 'AboutController',
+            templateUrl: 'views/viewAbout.html'
+        })
+        .otherwise({redirectTo: '/'});
 });
